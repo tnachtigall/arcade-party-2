d1b08f13b355f72d16bcc60fd4ce4c031b7adff6
LCLP <lukas@lclpnet.work>
2025-01-07 13:25:53 +0100

    add AP2_DEV env variable

--- src/main/java/work/lclpnet/ap2/base/ApConstants.java
+++ src/main/java/work/lclpnet/ap2/base/ApConstants.java
@@ -4,7 +4,7 @@ import net.fabricmc.loader.api.FabricLoader;
 
 public final class ApConstants {
 
-    public static final boolean DEVELOPMENT = FabricLoader.getInstance().isDevelopmentEnvironment();
+    public static final boolean DEVELOPMENT = FabricLoader.getInstance().isDevelopmentEnvironment() || "true".equals(System.getenv("AP2_DEV"));
     public static final String ID = "ap2";
 
     // people
